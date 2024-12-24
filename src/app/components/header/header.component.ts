e0df4a7630bc66e4dc778eed17bc85a665fcf738
Abdelkrim BECHIR <abdel.bechir@yahoo.fr>
2024-12-24 19:35:55 +0100

    gestion de la logique des menus en mode mobile

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -11,8 +11,19 @@ import { RouterLink } from '@angular/router';
 })
 export class HeaderComponent {
   isMenuOpen = false;
+  isProfileMenuOpen = false;
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+    if (this.isMenuOpen) {
+      this.isProfileMenuOpen = false;
+    }
+  }
+
+  toggleProfileMenu() {
+    this.isProfileMenuOpen = !this.isProfileMenuOpen;
+    if (this.isProfileMenuOpen) {
+      this.isMenuOpen = false;
+    }
   }
 }
