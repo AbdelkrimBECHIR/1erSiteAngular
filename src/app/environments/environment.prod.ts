7d98534a333350242276fdcc692534375632c577
Abdelkrim BECHIR <abdel.bechir@yahoo.fr>
2024-12-24 18:26:00 +0100

    suppression des fichiers environment du dossier local

--- src/app/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  weatherApiKey: 'e507a5017dfa47ad72775663a0aa1c76'
-};
--- src/app/environments/environment.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: false,
-  weatherApiKey: 'e507a5017dfa47ad72775663a0aa1c76'
-};
